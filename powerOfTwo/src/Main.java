e31717f8affd475258830ef72f833eefec65251a
YiuChoi <whucaiyao@gmail.com>
2016-05-17 15:10:21 +0800

    添加题目内容

--- powerOfTwo/src/Main.java
+++ powerOfTwo/src/Main.java
@@ -1,5 +1,7 @@
 /**
  * Created by 蔡小木 on 2016/5/17 0017.
+ * <p>
+ * Given an integer, write a function to determine if it is a power of two.
  */
 public class Main {
     public static void main(String[] args) {
--- twoSum/src/Main.java
+++ twoSum/src/Main.java
@@ -3,13 +3,23 @@ import java.util.HashMap;
 
 /**
  * Created by 蔡小木 on 2016/5/17 0017.
- *
- *  leetcode 第一题
+ * <p>
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution.
+ * <p>
+ * Example:
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * <p>
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * UPDATE (2016/2/13):
+ * The return format had been changed to zero-based indices. Please read the above updated description carefully.
  */
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(twoSum1(new int[]{2, 3, 7,4}, 6)));
+        System.out.println(Arrays.toString(twoSum1(new int[]{2, 3, 7, 4}, 6)));
     }
 
     //Runtime: 63 ms
